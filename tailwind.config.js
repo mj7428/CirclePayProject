79c94d9427f85b7028d435d16d2a5ce57a404dcb
MJ <134161638+mj7428@users.noreply.github.com>
2024-09-15 18:28:45 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#1A73E8',
+          secondary: '#34A853',
+        },
+        fontFamily: {
+          sans: ['Poppins', 'Arial', 'sans-serif'],
+        },
+      },
+    },
+  };
+  
